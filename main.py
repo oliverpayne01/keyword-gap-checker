2e90a6c2249d243ea872d88d7d1da1421b9e79f3
Oliver Payne <oliver@oliverpayne.me>
2025-12-12 21:11:10 -0700

    Import benchmark keyword list from csv and parse into data frame

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 from pathlib import Path
+import json
+
+# give the option to write path or choose default
 
 target_keywords_df = None
-target_keywords_csv_path = Path(input("Enter path to target keywords (.csv): "))
-
+target_keywords_csv_path = Path(
+    input("Enter the path to target keywords (must be a .csv): ")
+)
 if target_keywords_csv_path.suffix == ".csv":
     target_keywords_df = pd.read_csv(target_keywords_csv_path)
 else:
-    print(f"File type is not a .csv: {target_keywords_csv_path.suffix}")
+    print(f"File type '{target_keywords_csv_path.suffix}' is not a .csv:")
+
+# give the option to write path or choose default
 
 benchmark_keywords_df = None
-benchmark_keywords_csv_path = Path(input("Enter CSV: "))
+benchmark_keywords_csv_path = Path(
+    input("Enter the path to the keyword list you want to benchmark (must be a .csv): ")
+)
+if benchmark_keywords_csv_path.suffix == ".csv":
+    benchmark_keywords_df = pd.read_csv(benchmark_keywords_csv_path)
+else:
+    print(f"File type '{benchmark_keywords_csv_path.suffix}' is not a .csv")
 
 # Write default export location to a json file, and add to the .gitignore
