d4decef0828014833c695508a51ae9d3f82ca188
Oliver Payne <oliver@oliverpayne.me>
2025-12-14 13:42:04 -0700

    Begin refactoring states and logic in main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pandas as pd
 from pathlib import Path
 import json
 
-config = None
-
 
 def get_config():
     config_path = "./config.json"
@@ -13,49 +11,33 @@ def get_config():
         with open(config_path, "r") as json_file:
             return json.load(json_file)
     else:
-        new_config = {"target_keywords_path": "/test_path"}
-        with open(config_path, "w") as json_file:
-            json.dump(new_config, json_file)
-        return new_config
+        return None
 
 
-target_keywords_df = None
+config = get_config()
+
+# function to save
+# function to allow save
 
 
 def get_target_keywords():
-    config_path = Path("./config.json")
-    if config_path:
-        print(config_path)
-        # give the option to write a new path or use the default
+    # If we have a saved path, use that, otherwise input path and give option to save
+
+    if config["target_keywords_path"]:
+        
     else:
-        target_keywords_csv_path = Path(
-            input("Enter the path to target keywords (must be a .csv): ")
-        )
-        print("config path does not exist")
-
-    if target_keywords_csv_path.suffix == ".csv":
-        target_keywords_df = pd.read_csv(target_keywords_csv_path)
-    else:
-        print(f"File type '{target_keywords_csv_path.suffix}' is not a .csv:")
+        target_keywords_path = Path(input("Enter the path to target keywords (must be a .csv): "))
 
 
-# benchmark_keywords_df = None
-# benchmark_keywords_csv_path = Path(
-#     input("Enter the path to the keyword list you want to benchmark (must be a .csv): ")
-# )
-# if benchmark_keywords_csv_path.suffix == ".csv":
-#     benchmark_keywords_df = pd.read_csv(benchmark_keywords_csv_path)
-# else:
-#     print(f"File type '{benchmark_keywords_csv_path.suffix}' is not a .csv")
-
-# Write default export location to a json file, and add to the .gitignore
+# def analyze_gap(target_keywords_df, benchmark_keywords_df):
+#     # logic
+#     return
 
 
 def main():
-    global config
-    config = get_config()
-    print(config)
-    # get_target_keywords()
+    target_keywords_df = get_target_keywords()
+    #
+    # analyze_gap(get_target_keywords())
 
 
 if __name__ == "__main__":
