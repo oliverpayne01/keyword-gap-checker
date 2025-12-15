4f2604cf210953f0c3218a4caa09456f46fdadae
Oliver Payne <oliver@oliverpayne.me>
2025-12-14 20:11:39 -0700

    Add complete logic for loading, saving, and using a path for target keywords in main.py

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ from pathlib import Path
 import json
 
 
-def create_config(data):
+def create_config_file():
+    global config
+
     with open("config.json", "w") as json_file:
-        json.dump({}, json_file)
+        json.dump(config, json_file)
 
 
-# save config
+def save_config_file():
+    global config
+
+    with open("config.json", "w") as json_file:
+        json.dump(config, json_file)
 
 
 def set_config(key, value):
-    None
+    global config
+
+    if config != None:
+        config[key] = value
+    else:
+        config = {key: value}
 
 
-def load_config():
+def load_config_file():
     config_path = "./config.json"
     if os.path.exists(config_path):
         with open(config_path, "r") as json_file:
@@ -26,7 +37,7 @@ def load_config():
         return None
 
 
-config = load_config()
+config = load_config_file()
 # print(f"Config status: {config}")
 
 
@@ -36,14 +47,19 @@ def allow_save(key, value):
     ).upper()
 
     if allow_save_input == "Y":
-        if config == None:
-            create_config()
         set_config(key, value)
+
+        if config == None:
+            create_config_file()
+        else:
+            save_config_file()
 
 
 def get_target_keywords():
+    try:
+        return pd.read_csv(config["target_keywords_path"])
 
-    if config == None or not config["target_keywords_path"]:
+    except (KeyError, TypeError):
         target_keywords_path = None
 
         def handle_input():
@@ -59,7 +75,7 @@ def get_target_keywords():
 
             if os.path.exists(target_keywords_path):
                 if target_keywords_path.suffix == ".csv":
-                    allow_save("target_keywords_path", target_keywords_path)
+                    allow_save("target_keywords_path", str(target_keywords_path))
                     return
                 else:
                     print(f"File must be a csv ({target_keywords_path.suffix})")
@@ -70,6 +86,8 @@ def get_target_keywords():
 
         handle_input()
 
+        return pd.read_csv(target_keywords_path)
+
 
 # def analyze_gap(target_keywords_df, benchmark_keywords_df):
 #     # logic
@@ -78,6 +96,7 @@ def get_target_keywords():
 
 def main():
     target_keywords_df = get_target_keywords()
+    print(target_keywords_df.head())
     #
     # analyze_gap(get_target_keywords())
 
